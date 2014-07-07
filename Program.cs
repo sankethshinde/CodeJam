3f9ece167f620d96f9adc125f95d1d0d1e318faf
unknown <sshinde@LAB8.pune.tavisca.com>
2014-07-07 11:15:15 +0530

    Addition

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine(" Enter two nos: ");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            int Result = a + b;
+            Console.WriteLine(" Result of 2 nos is : " + Result);
+            Console.ReadLine();
+        }
+    }
+}
